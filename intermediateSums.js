855851f1c1b385079a0d5e9fdde8e7097ed4f269
Nicholas Rampton <nick.rampton@gmail.com>
2017-10-27 10:51:40 -0400

    Finished intermediateSums.js

---
+++ intermediateSums.js
@@ -0,0 +1,21 @@
+function intermediateSums(arr) {
+  var counter = 0;
+  var length = arr.length;
+  var sum = 0;
+  for (var i = arr.length-1; (i-1) %10 !== 0; i--) {
+    sum += arr[i];
+  }
+  arr[arr.length] = sum;
+  while ( counter * 10 +1 < length) {
+    sum = 0;
+    for ( i = counter*10 + counter; i < (counter+1)*10 + counter; i++) {
+      sum += arr[i];
+    }
+    for (var j = arr.length; j > (counter+1)*10 + counter; j--) {
+      arr[j] = arr[j-1];
+    }
+    arr[(counter+1)*10 + counter] = sum;
+    counter++;
+  }
+  return arr;
+}
